00b30c76de7595f9d8a20537722ff44348752936
mohamed-ibrahim-01 <mohamed.ibrahim02023@gmail.com>
2021-01-24 19:49:43 +0200

    installing and configure the middle ware

--- server/server.js
+++ server/server.js
@@ -1,15 +1,26 @@
-// Setup empty JS object to act as endpoint for all routes
 // Express to run server and routes
+const express = require("express");
+const cors = require('cors');
+const body_parser = require('body-parser');
 
 // Start up an instance of app
+const app = express();
+// Setup empty JS object to act as endpoint for all routes
 
 /* Dependencies */
 /* Middleware*/
 
 //Here we are configuring express to use body-parser as middle-ware.
-// Cors for cross origin allowance
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+// Cors for cross origin allowance
+app.use(cors());
 // Initialize the main project folder
+app.use(express.static("../app"));
 
 // Spin up the server
 // Callback to debug
@@ -19,4 +30,3 @@
 // Callback function to complete GET '/all'
 
 // Post Route
-  
