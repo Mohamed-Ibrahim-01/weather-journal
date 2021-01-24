84d12ac7fba3bc0e3946d74c8282b482e2283c80
mohamed-ibrahim-01 <mohamed.ibrahim02023@gmail.com>
2021-01-24 21:03:09 +0200

    install dotenv

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+//dotenv to use enviroment variables 
+require('dotenv').config();
 // Express to run server and routes
 const express = require("express");
 const cors = require('cors');
@@ -5,6 +7,9 @@ const body_parser = require('body-parser');
 
 // Start up an instance of app
 const app = express();
+
+//get enviroment variables (PORT)
+const { PORT, HOST } = process.env;
 // Setup empty JS object to act as endpoint for all routes
 
 /* Dependencies */
@@ -13,16 +18,22 @@ const app = express();
 //Here we are configuring express to use body-parser as middle-ware.
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(body_parser.urlencoded({ extended: false }))
 // parse application/json
-app.use(bodyParser.json())
+app.use(body_parser.json())
 
 // Cors for cross origin allowance
 app.use(cors());
 // Initialize the main project folder
 app.use(express.static("../app"));
 
+//
+function listening (){
+    console.log(`Server is listening in port ${PORT}`);
+}
+
 // Spin up the server
+app.listen(PORT, listening);
 // Callback to debug
 
 // Initialize all route with a callback function
