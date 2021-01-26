868ff180f91ab075927086e07eb0044c5103390e
Mohamed-Ibrahim-01 <60495252+Mohamed-Ibrahim-01@users.noreply.github.com>
2021-01-27 00:01:56 +0200

    remove server side old comments

--- server/server.js
+++ server/server.js
@@ -1,44 +1,31 @@
-//dotenv to use enviroment variables 
 require('dotenv').config();
-// Express to run server and routes
-const express = require("express");
+const express = require('express');
 const cors = require('cors');
 const body_parser = require('body-parser');
 
-// Start up an instance of app
 const app = express();
+const { PORT } = process.env;
 
-//get enviroment variables (PORT)
-const { PORT, HOST } = process.env;
-// Setup empty JS object to act as endpoint for all routes
+let projectData = { inputData: {}, weatherData: {} };
 
-// Gettnig the Weather Route
-const weatherRoute = require('weatherRouter');
-/* Dependencies */
-/* Middleware*/
-
-//Here we are configuring express to use body-parser as middle-ware.
-
-// parse application/x-www-form-urlencoded
-app.use(body_parser.urlencoded({ extended: false }))
-// parse application/json
-app.use(body_parser.json())
-
-// Cors for cross origin allowance
+app.use(body_parser.urlencoded({ extended: false }));
+app.use(body_parser.json());
 app.use(cors());
-// Initialize the main project folder
-app.use(express.static("../app"));
+app.use(express.static('../app'));
 
 // Spin up the server
-function listening (){
+function listening() {
     console.log(`Server is listening in port ${PORT}`);
 }
 app.listen(PORT, listening);
-// Callback to debug
-
-// Initialize all route with a callback function
-app.use(weatherRoute);
-
-// Callback function to complete GET '/all'
-
-// Post Route
+app.get('/getProjectData', (req, res) => {
+    res.send(projectData);
+});
+app.post('/storeInput', (req, res) => {
+    projectData.inputData = req.body;
+    res.send({ msg: 'Data Stored' });
+});
+app.post('/storeWeather', (req, res) => {
+    projectData.weatherData = req.body;
+    res.send({ msg: 'Data Stored' });
+});
